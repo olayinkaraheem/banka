9065467390c98abee7168d9a9f0273125eb90d08
kareem Olayinka <olayinka.raheem16@gmail.com>
2019-03-29 03:30:23 +0100

    [created transaction history page] feature/clienttransactionhistory

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,9 @@
 const body = document.querySelector('body');
 const menu_icon = document.querySelector('#menu-icon');
 const navigation = document.querySelector('.navigation');
+const dashboard_menu = document.querySelector('#dashboard-menu-item-container');
+const menu_open_btn = document.querySelector('#open-btn');
+const menu_close_btn = document.querySelector('#close-btn');
 
 // body.addEventListener('click', () => {
 //     if(navigation.classList.contains('visible')){
@@ -11,4 +14,21 @@ const navigation = document.querySelector('.navigation');
 
 menu_icon.addEventListener('click', () => {
     navigation.classList.toggle('visible');
+})
+
+
+menu_open_btn.addEventListener('click', (event) => {
+    menu_open_btn.classList.add('hide');
+    if (dashboard_menu.classList.contains('hide-menu')) {
+        dashboard_menu.classList.remove('hide-menu');
+    }
+    dashboard_menu.classList.add('show-menu');
+    // menu_close_btn.classList.add('show');
+})
+
+menu_close_btn.addEventListener('click', (event) => {
+    // menu_open_btn.classList.add('hide');
+    dashboard_menu.classList.remove('show-menu');
+    dashboard_menu.classList.add('hide-menu');
+    // menu_close_btn.classList.add('show');
 })
