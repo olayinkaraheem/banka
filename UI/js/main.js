dd0104bbac21a2550cf498fa6cc444e15b2a3ae0
kareem Olayinka <olayinka.raheem16@gmail.com>
2019-03-29 22:47:58 +0100

    [fixed dashboard menu toggle button] fix/adminmenutoggleicon

--- UI/js/main.js
+++ UI/js/main.js
@@ -5,30 +5,51 @@ const dashboard_menu = document.querySelector('#dashboard-menu-item-container');
 const menu_open_btn = document.querySelector('#open-btn');
 const menu_close_btn = document.querySelector('#close-btn');
 
-// body.addEventListener('click', () => {
-//     if(navigation.classList.contains('visible')){
-//         navigation.classList.remove('visible');
-//     }
-//     console.log('body clicked');
-// })
 
+// Toggle between class 'visible' for nav menu
 menu_icon.addEventListener('click', () => {
     navigation.classList.toggle('visible');
 })
 
+// Toggle between dashboard open and close buttons
+if(menu_open_btn){      
+    menu_open_btn.addEventListener('click', (event) => {
+        const elem = event.target;
+        if(event.target.id == 'open-btn' ) {
+            elem.setAttribute('id', 'close-btn');
+            elem.innerHTML = '&times;';
+            // console.log(elem.style.top); 
+            if (dashboard_menu.classList.contains('hide-menu')) {
+                dashboard_menu.classList.remove('hide-menu');
+            }
+            dashboard_menu.classList.add('show-menu');
+        } else {
+            elem.setAttribute('id', 'open-btn');
+            elem.innerHTML = '&plus;';
+            if (dashboard_menu.classList.contains('show-menu')) {
+                dashboard_menu.classList.remove('show-menu');
+            }
+            dashboard_menu.classList.add('hide-menu');
+        }
+        // menu_close_btn.classList.add('show');
+    })
+}
 
-menu_open_btn.addEventListener('click', (event) => {
-    menu_open_btn.classList.add('hide');
-    if (dashboard_menu.classList.contains('hide-menu')) {
-        dashboard_menu.classList.remove('hide-menu');
-    }
-    dashboard_menu.classList.add('show-menu');
-    // menu_close_btn.classList.add('show');
-})
 
-menu_close_btn.addEventListener('click', (event) => {
-    // menu_open_btn.classList.add('hide');
-    dashboard_menu.classList.remove('show-menu');
-    dashboard_menu.classList.add('hide-menu');
-    // menu_close_btn.classList.add('show');
-})
+
+// menu_close_btn.addEventListener('click', (event) => {
+//     event.target.setAttribute('id', 'open-btn');
+//     event.target.innerHTML = '&plus;';
+//     if (dashboard_menu.classList.contains('show-menu')) {
+//         dashboard_menu.classList.remove('show-menu');
+//     }
+//     dashboard_menu.classList.add('hide-menu');
+//     // menu_close_btn.classList.add('show');
+// })
+
+// menu_close_btn.addEventListener('click', (event) => {
+//     // menu_open_btn.classList.add('hide');
+//     dashboard_menu.classList.remove('show-menu');
+//     dashboard_menu.classList.add('hide-menu');
+//     // menu_close_btn.classList.add('show');
+// })
