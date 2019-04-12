e44e267930438094283878105b6d335355d2a4dd
kareem Olayinka <olayinka.raheem16@gmail.com>
2019-04-12 11:03:33 +0100

    chore(test) Set AccountStatus test [Finishes #165199396]

--- api/src/tests/accounts.test.js
+++ api/src/tests/accounts.test.js
@@ -35,4 +35,56 @@ describe('Accounts', () => {
         });
     });
   });
+
+  describe('/PATCH Account Activate Deactivate', () => {
+    it('It should set a bank account to dormant status', done => {
+      const account_status = {
+        status: 'dormant'
+      };
+      chai
+        .request(app)
+        .post('/api/v1/account/1233445642')
+        .send(account_status)
+        .end((err, res) => {
+          expect(account_status).to.have.property('status');
+          res.should.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.have.property('id');
+          expect(res.body.data).to.have.property('status');
+          expect(res.body.data.status).to.equal('dormant');
+          expect(res.body.data).to.have.property('type');
+          expect(res.body.data).to.have.property('owner');
+          expect(res.body.data).to.have.property('firstName');
+          expect(res.body.data).to.have.property('lastName');
+          expect(res.body.data).to.have.property('accountNumber');
+          expect(res.body.data).to.have.property('email');
+          done();
+        });
+    });
+
+    it('It should set a bank account to active status', done => {
+      const account_status = {
+        status: 'active'
+      };
+      chai
+        .request(app)
+        .post('/api/v1/account/1233445642')
+        .send(account_status)
+        .end((err, res) => {
+          expect(account_status).to.have.property('status');
+          res.should.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.have.property('id');
+          expect(res.body.data).to.have.property('status');
+          expect(res.body.data.status).to.equal('active');
+          expect(res.body.data).to.have.property('type');
+          expect(res.body.data).to.have.property('owner');
+          expect(res.body.data).to.have.property('firstName');
+          expect(res.body.data).to.have.property('lastName');
+          expect(res.body.data).to.have.property('accountNumber');
+          expect(res.body.data).to.have.property('email');
+          done();
+        });
+    });
+  });
 });
