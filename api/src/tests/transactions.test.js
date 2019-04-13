af39e3b3eeb7b677443fc32f78ba0db929566a8f
kareem Olayinka <olayinka.raheem16@gmail.com>
2019-04-13 11:16:06 +0100

    chore(test): create creditaccount test [Finishes #165164168]

--- api/src/tests/transactions.test.js
+++ api/src/tests/transactions.test.js
@@ -9,7 +9,7 @@ const should = chai.should();
 chai.use(chaiHttp);
 describe('Transactions', () => {
   describe('/POST Perform Transaction', () => {
-    it('It should debit bank account', done => {
+    it('It should debit a bank account', done => {
       const transaction_detail = {
         amount: parseFloat('3000.00'),
         cashier: 2,
@@ -19,6 +19,33 @@ describe('Transactions', () => {
       chai
         .request(app)
         .post('/api/v1/transaction/1233445643/debit')
+        .send(transaction_detail)
+        .end((err, res) => {
+          expect(transaction_detail).to.have.property('type');
+          expect(transaction_detail).to.have.property('cashier');
+          expect(transaction_detail).to.have.property('amount');
+          res.should.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.have.property('transactionId');
+          expect(res.body.data).to.have.property('amount');
+          expect(res.body.data).to.have.property('cashier');
+          expect(res.body.data).to.have.property('transactionType');
+          expect(res.body.data).to.have.property('accountNumber');
+          expect(res.body.data).to.have.property('accountBalance');
+          done();
+        });
+    });
+
+    it('It should credit a bank account', done => {
+      const transaction_detail = {
+        amount: parseFloat('3000.00'),
+        cashier: 2,
+        type: 'credit'
+      };
+
+      chai
+        .request(app)
+        .post('/api/v1/transaction/1233445643/credit')
         .send(transaction_detail)
         .end((err, res) => {
           expect(transaction_detail).to.have.property('type');
